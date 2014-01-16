3317515c61f3d2afab18209e9030d992976c8480
Simon de Haan <simon@praekeltfoundation.org>
2014-01-16 18:24:53 +0200

    package

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="txssmi",
+    version="0.1",
+    url='http://github.com/smn/txssmi',
+    license='BSD',
+    description="Twisted library for Truteq's SSMI protocol",
+    long_description=open('README.rst', 'r').read(),
+    author='Simon de Haan',
+    author_email='simon@praekeltfoundation.org',
+    packages=[
+        "txssmi",
+    ],
+    package_data={},
+    include_package_data=True,
+    install_requires=[
+        "Twisted",
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Framework :: Twisted',
+    ],
+)
