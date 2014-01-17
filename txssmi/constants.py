870c730f4688331e657fb002af19bf3e0e2a4420
Simon de Haan <simon@praekeltfoundation.org>
2014-01-17 15:46:23 +0200

    authenticate + test

--- txssmi/constants.py
+++ txssmi/constants.py
@@ -1,7 +1,8 @@
-SSMI_HEADER = u"SSMI"
+SSMI_HEADER = "SSMI"
 
 COMMANDS = {
-    u"1": ("LOGIN", ["username", "password"]),
+    "1": ("LOGIN", ["username", "password"]),
+    "101": ("ACK", ["ack_type"]),
 }
 
 
@@ -11,35 +12,7 @@ COMMAND_IDS = dict([(value, key)
                     for key, value in COMMAND_NAMES.items()])
 COMMAND_FIELDS = dict(value for key, value in COMMANDS.items())
 
-
-SSMI_USSD_TYPE_NEW = u"1"
-SSMI_USSD_TYPE_EXISTING = u"2"
-SSMI_USSD_TYPE_END = u"3"
-SSMI_USSD_TYPE_TIMEOUT = u"4"
-SSMI_USSD_TYPE_REDIRECT = u"5"
-SSMI_USSD_TYPE_NI = u"6"
-
-ack_reason = {
-    "1": "Login OK",
-    "2": "Link check response",
+ACK_TYPES = {
+    "LOGIN_OK": "1",
+    "LINK_CHECK_RESPONSE": "2",
 }
-
-nack_reason = {
-    "1": "Invalid username/password combination",
-    "2": "Invalid/unknown message type",
-    "3": "Could not parse message",
-    "4": "Account suspended (non-payment or abuse)",
-}
-
-# "SEQ": u"100",
-# "TEXT_MESSAGE": u"103",
-# "DELIVERY_MESSAGE": u"104",
-# "FREEFORM_MESSAGE": u"105",
-# "BINARY_MESSAGE": u"106",
-# "PREMIUMRATED_MESSAGE": u"107",
-# "BINARY_PREMIUMRATED_MESSAGE": u"108",
-# "USSD_EXTENDED": u"111",
-# "EXTENDED_RETURN": u"113",
-# "EXTENDED_RETURN_BINARY": u"116",
-# "EXTENDED_PREMIUMRATED_MESSAGE": u"117",
-# "EXTENDED_BINARY_PREMIUMRATED_MESSAGE": u"118",
--- txssmi/protocol.py
+++ txssmi/protocol.py
@@ -1,13 +1,13 @@
 # -*- test-case-name: txssmi.tests.test_protocol -*-
 
-from functools import partial
-
+from twisted.internet.defer import maybeDeferred, succeed, DeferredQueue
 from twisted.protocols.basic import LineReceiver
 from twisted.python import log
 
 from smspdu import gsm0338
 
-from txssmi.commands import Login
+from txssmi.commands import Login, SSMICommand
+from txssmi.constants import COMMAND_IDS, ACK_TYPES
 
 gsm = gsm0338()
 
@@ -17,13 +17,31 @@ class SSMIProtocol(LineReceiver):
     delimeter = '\r'
     noisy = False
 
+    def __init__(self):
+        self.command_queue = DeferredQueue()
+
     def emit(self, prefix, msg):
         if self.noisy:
             log.msg('%s %s' % (prefix, msg))
 
+    def lineReceived(self, line):
+        cmd = SSMICommand.parse(line)
+        handler = getattr(self, 'handle_%s' % (cmd.command_name,))
+        return maybeDeferred(handler, cmd)
+
     def send_command(self, command):
         self.emit('>>', '%s: %s' % (command.command_name, command.values))
-        return self.sendLine(str(command))
+        return succeed(self.sendLine(str(command)))
 
     def login(self, username, password):
-        self.send_command(Login(username=username, password=password))
+        return self.send_command(Login(username=username, password=password))
+
+    def authenticate(self, username, password):
+        d = self.login(username, password)
+        d.addCallback(lambda _: self.command_queue.get())
+        d.addCallback(lambda cmd: (cmd.command_id == COMMAND_IDS['ACK'] and
+                                   cmd.ack_type == ACK_TYPES['LOGIN_OK']))
+        return d
+
+    def handle_ACK(self, ack):
+        return self.command_queue.put(ack)
