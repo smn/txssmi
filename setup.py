10a7689cf7c83ec2e35b5cc7bd213af664eea444
Jeremy Thurgood <firxen@gmail.com>
2014-08-20 14:29:24 +0200

    Bump version to 0.3.1a post-release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="txssmi",
-    version="0.3.0",
+    version="0.3.1a",
     url='http://github.com/smn/txssmi',
     license='BSD',
     description="Twisted library for Truteq's SSMI protocol",
